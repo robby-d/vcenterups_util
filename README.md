0ded07f9b54fe395c48701f307d5c3312c0ef189
robby-dermody <robbyd@u20.org>
2022-11-16 21:22:40 -0500

    added support for tripplite UPSes, and moved from standalone esxi hosts to work with vcenter instead

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # cpesxi_util
 
-Simple script to trigger an ESXi server shutdown if a Cyberpower UPS registers a low battery state. Created due to lack of ESXi 6.7 support with Cyberpower PowerPanel Business Edition for Virtual Machines.
+Simple script to trigger an VMware vCenter deployment shutdown (i.e. of the vcenter server vm and all vms it manages) if a Tripplite or Cyberpower UPS gets below a certain percentage battery remaining.
 
 ## Requirements
 
-* A Cyberpower UPS with an RMCARD remote access module
-* RMCARD assigned an IP address and SNMP v1 enabled via the web GUI
-* ESXi server (tested with ESXi 6.7)
+* A Tripplite UPS with a SNMPWEBCARD device, or a Cyberpower UPS with an RMCARD remote access module
+* UPS assigned an IP address and SNMP v1 enabled via the web GUI
+* VMware vCenter host managing 1 or more ESXi systems (tested with vCenter 7)
 
 ## Installation
 
@@ -21,32 +21,26 @@ cd
 git clone git@github.com:robby-dermody/cpesxi_util.git
 ```
 
-### Install vSphere CLI tools
+### Install `cpesxi_util`  dependencies
 
-Download vSphere Linux x86_64 CLI utilities from [here](https://my.vmware.com/web/vmware/details?downloadGroup=VS-CLI-670&productId=742).
-
-Then:
-```
-tar -zxvf VMware-vSphere-CLI-*.x86_64.tar.gz
-sudo apt install -y build-essential perl-doc libmodule-build-perl libssl-dev libxml-libxml-perl libsoap-lite-perl libuuid-perl libcrypt-ssleay-perl libarchive-zip-perl libsocket6-perl libio-socket-inet6-perl libnet-inet6glue-perl
-sudo ./vmware-vsphere-cli-distrib/vmware-install.pl
-```
-
-Then, to fix a perl dependency issue, do the following:
-```
-sudo cpan
-install GAAS/libwww-perl-5.837.tar.gz
-exit
-```
-
-### Install other `cpesxi_util`  dependencies
 ```
 sudo apt-get install git python3 python3-pip
 sudo apt-get install libsnmp-dev snmp-mibs-downloader
 sudo pip3 install easysnmp
 ```
 
-### (Optional) Create a new admin user on your ESXi server 
+### Create a role, user and mapping in vCenter
+
+In vCenter, under Administration -> Roles, create a new user role called `cpesxi Users` and give it the following permissions:
+```
+Virtual Machine -> Power off
+Virtual Machine -> Power on
+```
+
+Then under Administration -> Users and Groups, create a new user named `cpesxi_user` and assign it a secure password.
+
+Finally, under Administration -> Global Permissions, add a mapping entry for `cpesxi_user` to assign it the `cpesxi Users` role. Make sure `Propagate to children` is checked.
+
 
 ### Copy the config file
 
@@ -54,15 +48,30 @@ Copy `~/cpesxi_util/cpesxi_util.yaml.orig` configuration file template to `~/cpe
 
 ```
 deployment_NAME:
-  esxi_host: ESXI server IP/hostname
-  esxi_username: ESXI server admin user username
-  esxi_password: ESXI user password
-  ups_host: UPS RMCARD IP/hostname
+  vcenter_host: vcenter server IP/hostname
+  vcenter_username: vcenter server admin user username
+  vcenter_password: vcenter user password
+  vcenter_vm_name: The VM name of the vcenter server (e.g. vcenter, vcenter01, etc)
+  executing_host_vm_name: If this script is running on a VM managed by this vcenter platform, put the name of that VM here
+  ups_type: "tripplite" or "cyberpower"
+  ups_host: UPS IP/hostname
   ups_snmpv1_community: SNMP community (normally "public")
-  initiate_shutdown_at_batt_pct_remaining: when the % UPS battery remaining gets to or under this level, the script will shutdown the ESXi server
+  initiate_shutdown_at_batt_pct_remaining: when the % UPS battery remaining gets to or under this level, the script will shutdown the vcenter deployment
 ```
 
-Multiple deployment sections can be added to support multiple ESXi server/UPS combinations.
+Multiple deployment sections can be added to support multiple vcenter system/UPS combinations.
+
+Once done creating this file, properly set its permissions:
+```
+chmod 600 cpesxi_util.yaml
+```
+
+### Allow shutdown command access from non-root user
+The script needs to be able to shutdown the system from the command line without running as root. To allow this, run the following command, replacing `MY_USER` with the user you intend to run the script as:
+
+```
+sudo bash -c "echo 'MY_USER ALL = (root) NOPASSWD: /usr/sbin/shutdown' > /etc/sudoers.d/cpesxi_util"
+```
 
 ### DRY RUN TEST
 
--- cpesxi_util.py
+++ cpesxi_util.py
@@ -16,18 +16,61 @@ import time
 import yaml
 import json
 import subprocess
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 from easysnmp import Session
 
 PROG_NAME = "cpesxi_util"
 ESXI_SHUTDOWN_MIN_REPEAT_PERIOD = 3600 # 1 hour
 
-#https://www.reddit.com/r/homelab/comments/5pdxwb/cyberpower_ups_and_grafana_now_with_snmp/
-CYBERPOWER_OID_BATTERY_TIME_LEFT_TICKS = "1.3.6.1.4.1.3808.1.1.1.2.2.4.0"
-CYBERPOWER_OID_BATTERY_CAPACITY_LEFT_PCT = "1.3.6.1.4.1.3808.1.1.1.2.2.1.0"
-CYBERPOWER_OID_CURRENT_INPUT_VOLTAGE = "1.3.6.1.4.1.3808.1.1.1.3.2.1.0"
-
 logger = None
+
+def get_vc_session(vc_hostname, username, password):
+    '''get the vCenter server session'''
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    s = requests.Session()
+    s.verify = False   # disable checking server certificate
+    s.auth = (username, password) # basic auth
+    try:
+        r = s.post("https://{}/api/session".format(vc_hostname))
+    except requests.exceptions.ConnectionError:
+        logger.error("Error connecting to vCenter {}".format(vc_hostname))
+        return False
+    #logger.debug(r.headers)
+    #logger.debug(r.request.headers)
+    if r.status_code != 201:
+        logger.error("Got error status code {} from vcenter server: {}".format(r.status_code, r.text))
+        return False
+    logger.debug("Got vCenter session ID {}".format(r.headers['vmware-api-session-id']))
+    s.headers.update({'vmware-api-session-id': r.headers['vmware-api-session-id']})
+    return s
+
+def get_vm_list(s, vc_hostname):
+    '''Function to get all the VMs from vCenter inventory'''   
+    r = s.get("https://" + vc_hostname + "/api/vcenter/vm")
+    if r.status_code != 200:
+        logger.error("List VMs failed. Status code: {}, error: {}".format(r.status_code, r.text))
+        return False
+    return json.loads(r.text)
+
+def get_vm_poweredon_list(s, vc_hostname):
+    '''Function to get all the VMs from vCenter inventory that are powered on'''
+    vm_query_params = {"power_states":["POWERED_ON"]}
+    r = s.get("https://" + vc_hostname + "/api/vcenter/vm", params = vm_query_params)
+    if r.status_code != 200:
+        logger.error("List VMs status_code: " + str(json_vm_list.status_code))
+        return False
+    return json.loads(r.text)
+
+def guest_shutdown(s, vmid, vc_hostname):
+    '''Shut down guest VM'''
+    vm_action = {"action": "shutdown"}
+    r = s.post("https://" + vc_hostname + "/api/vcenter/vm/" + vmid + "/guest/power", params = vm_action)
+    if r.status_code != 200:
+        logger.error("Shutdown VM status_code: " + str(json_vm_list.status_code))
+        return False
+    return r
 
 def get_program_dir():
     return os.path.realpath(os.path.dirname(__file__))
@@ -52,18 +95,116 @@ def set_up_logging(debug):
 
 def load_config():
     with open(os.path.join(get_program_dir(), "{}.yaml".format(PROG_NAME)), 'r') as ymlfile:
-        config = yaml.load(ymlfile)
+        config = yaml.safe_load(ymlfile)
     for section in config:
         if not section.startswith('deployment_'):
             logger.error("Invalid config section {}".format(section))
             return False
-        for p in ('esxi_host', 'esxi_username', 'esxi_password', 'ups_host', 'ups_snmpv1_community', 'initiate_shutdown_at_batt_pct_remaining'):
+        #required fields
+        for p in ('vcenter_host', 'vcenter_username', 'vcenter_password', 'vcenter_vm_name',
+                  'ups_type', 'ups_host', 'ups_snmpv1_community', 'initiate_shutdown_at_batt_pct_remaining'):
             if p not in config[section] or not config[section][p]:
                 logger.error("Missing or empty property '{}'".format(p))
                 return False
+        if 'executing_host_vm_name' not in config[section]:
+            config[section]['executing_host_vm_name'] = ''
+        
+        assert config[section]['ups_type'] in ('tripplite', 'cyberpower')
     return config
 
-def main():
+def get_ups_stats(deployment_config):
+    # Triplite OIDs, from: https://assets.tripplite.com/flyer/supported-snmp-oids-technical-application-bulletin-en.pdf
+    TRIPPLITE_OID_BATTERY_TIME_LEFT_MIN = ".1.3.6.1.2.1.33.1.2.3.0"  # UPSEstimatedMinutesRemaining
+    TRIPPLITE_OID_BATTERY_CAPACITY_LEFT_PCT = ".1.3.6.1.2.1.33.1.2.4.0"  # UPSEstimatedChargeRemaining
+    TRIPPLITE_OID_CURRENT_INPUT_VOLTAGE = ".1.3.6.1.2.1.33.1.3.3.1.3.1"  # UPSInputVoltage
+    #Cyberpower UPS OIDs, from: https://www.reddit.com/r/homelab/comments/5pdxwb/cyberpower_ups_and_grafana_now_with_snmp/
+    CYBERPOWER_OID_BATTERY_TIME_LEFT_TICKS = ".1.3.6.1.4.1.3808.1.1.1.2.2.4.0"
+    CYBERPOWER_OID_BATTERY_CAPACITY_LEFT_PCT = ".1.3.6.1.4.1.3808.1.1.1.2.2.1.0"
+    CYBERPOWER_OID_CURRENT_INPUT_VOLTAGE = ".1.3.6.1.4.1.3808.1.1.1.3.2.1.0"
+
+    # read state from UPS
+    stats = {}
+    logger.debug("Reading power state from {} UPS @ {} (SNMP: {})".format(
+        deployment_config['ups_type'], deployment_config['ups_host'], deployment_config['ups_snmpv1_community']))
+    snmp_session = Session(hostname=deployment_config['ups_host'], community=deployment_config['ups_snmpv1_community'], version=1)
+
+    if deployment_config['ups_type'] == 'tripplite':        
+        stats['ups_input_voltage'] = int(snmp_session.get(TRIPPLITE_OID_CURRENT_INPUT_VOLTAGE).value)
+        stats['ups_is_discharging'] = not bool(stats['ups_input_voltage'])  # voltage is 0 when unit is discharging
+        stats['ups_min_left'] = int(snmp_session.get(TRIPPLITE_OID_BATTERY_TIME_LEFT_MIN).value)
+        stats['ups_pct_left'] = float(snmp_session.get(TRIPPLITE_OID_BATTERY_CAPACITY_LEFT_PCT).value)
+    else:
+        assert deployment_config['ups_type'] == 'cyberpower'
+        stats['ups_input_voltage'] = int(snmp_session.get(CYBERPOWER_OID_CURRENT_INPUT_VOLTAGE).value)
+        stats['ups_is_discharging'] = not bool(stats['ups_input_voltage'])  # voltage is 0 when unit is discharging
+        stats['ups_min_left'] = int(snmp_session.get(CYBERPOWER_OID_BATTERY_TIME_LEFT_TICKS).value) / 6000.0
+        stats['ups_pct_left'] = float(snmp_session.get(CYBERPOWER_OID_BATTERY_CAPACITY_LEFT_PCT).value)
+    return stats
+
+def do_vcenter_shutdown(deployment_config, is_dry_run):
+    MAX_SHUTDOWN_WAIT_COUNT = 10
+    SECONDS_BETWEEN_SHUTDOWN_AND_CHECK = 15
+    #VCENTER_SHUTDOWN_WAIT_TIME_SECONDS = 100
+
+    #Get vCenter server session
+    s = get_vc_session(deployment_config['vcenter_host'], deployment_config['vcenter_username'], deployment_config['vcenter_password'])
+    if s is False:
+        return False
+
+    # Get list of VMs powered on
+    vm_list = get_vm_poweredon_list(s, deployment_config['vcenter_host'])
+    if vm_list is False:
+        return False
+
+    logger.info("Seeing {} VMs".format(len(vm_list)))
+    logger.debug("Full VM list: {}".format(vm_list))
+
+    filtered_vm_list = [vm for vm in vm_list if vm["name"] not in (deployment_config['vcenter_vm_name'], deployment_config['executing_host_vm_name'])]
+    vcenter_vm_list = [vm for vm in vm_list if vm["name"] == deployment_config['vcenter_vm_name']]
+
+    assert len(vcenter_vm_list) in (0, 1)
+    if len(vcenter_vm_list) == 0:
+        logger.error("Cannot find vcenter server named {}".format(deployment_config['vcenter_vm_name']))
+    
+    # Reorder the list with the vcenter server last, leaving out the vm host executing this script (if there is one), which will be shutdown via shell command
+    vm_list_to_shutdown = filtered_vm_list + vcenter_vm_list  # leave out executing vm host
+    logger.info("Shutting down the following VMs: {}".format(', '.join([vm['name'] for vm in vm_list_to_shutdown])))
+
+    # shut down non-vcenter vms first
+    for vm in filtered_vm_list:
+        logger.info("Shutting down {} ({}) (dry run: {})...".format(vm["name"], vm["vm"], is_dry_run))
+        if not is_dry_run:
+            guest_shutdown(s, vm["vm"], deployment_config['vcenter_host'])  # todo: handle API call error?
+
+    if not is_dry_run:
+        for i in range(MAX_SHUTDOWN_WAIT_COUNT):
+            logger.info("Waiting {} seconds before checking VM powered on status (try {} of {})...".format(
+                SECONDS_BETWEEN_SHUTDOWN_AND_CHECK, i + 1, MAX_SHUTDOWN_WAIT_COUNT))
+            time.sleep(SECONDS_BETWEEN_SHUTDOWN_AND_CHECK)
+            # See if any VMs still powered on (excluding vcenter and executing_host_vm_name, if any)
+            vms_powered_on = get_vm_poweredon_list(s, deployment_config['vcenter_host'])
+            if vms_powered_on is False:
+                return False
+            #remove vcenter and executing_host_vm_name from the result
+            vms_powered_on = [vm for vm in vms_powered_on if vm['name'] not in (deployment_config['vcenter_vm_name'], deployment_config['executing_host_vm_name'])]
+            if len(vms_powered_on):
+                logger.info("Still waiting on {} VMs to shut down: {}".format(len(filtered_vm_list), ','.join([vm['name'] for vm in filtered_vm_list])))
+            else: # all done with VM shutdown
+                break
+        else:
+            logger.error("Did not finish VM shutdowns in the allowed time, aborting.")
+            return False
+
+    # now shut down vcenter
+    logger.info("Shutting down vcenter server {} (dry run: {})...".format(vcenter_vm_list[0]['name'], is_dry_run))
+    if not is_dry_run:
+        guest_shutdown(s, vm["vm"], deployment_config['vcenter_host'])    
+        #time.sleep(VCENTER_SHUTDOWN_WAIT_TIME_SECONDS) # wait a certain amount of time before assuming vcenter is shut down
+
+    return True
+
+
+def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action='store_true', default=False, help="increase output verbosity")
     parser.add_argument("--dry-run", action='store_true', default=False, help="dry run only (don't trigger shutdown)")
@@ -84,28 +225,17 @@ def main():
     else:
         state = {}
 
-    # for each UPS-ESXi combo
+    # for each UPS-vcenter deployment combo...
     for deployment in config:
         logger.info("Processing {}".format(deployment))
-        state.setdefault(deployment, {})
+        state.setdefault(deployment, {'success': None})
         state[deployment].setdefault('shutdown-times', [])
             
-        # read power state from UPS
-        logger.debug("Reading power state from UPS @ {} (SNMP: {})".format(
-            config[deployment]['ups_host'], config[deployment]['ups_snmpv1_community']))
-        session = Session(hostname=config[deployment]['ups_host'], community=config[deployment]['ups_snmpv1_community'], version=1)
-        
-        ups_input_voltage = session.get('.' + CYBERPOWER_OID_CURRENT_INPUT_VOLTAGE)
-        ups_input_voltage = int(ups_input_voltage.value)
-        ups_is_discharging = not bool(ups_input_voltage)  # voltage is 0 when unit is discharging
-
-        ups_ticks_left = session.get('.' + CYBERPOWER_OID_BATTERY_TIME_LEFT_TICKS)
-        ups_min_left = int(ups_ticks_left.value) / 6000.0
-        ups_pct_left = float(session.get('.' + CYBERPOWER_OID_BATTERY_CAPACITY_LEFT_PCT).value)
+        ups_stats = get_ups_stats(config[deployment])
 
         logger.info("UPS capacity has {}% remaining (> {}% allowed) ({} minutes runtime left, UPS {} discharging)".format(
-                ups_pct_left, config[deployment]['initiate_shutdown_at_batt_pct_remaining'], ups_min_left,
-                "IS" if ups_is_discharging else "IS NOT"))
+                ups_stats['ups_pct_left'], config[deployment]['initiate_shutdown_at_batt_pct_remaining'], ups_stats['ups_min_left'],
+                "IS" if ups_stats['ups_is_discharging'] else "IS NOT"))
 
         now_epoch = int(time.time())
 
@@ -113,47 +243,48 @@ def main():
         if len(state[deployment]['shutdown-times']):
             last_shutdown = state[deployment]['shutdown-times'][-1]
             assert last_shutdown < now_epoch
-            if now_epoch - last_shutdown < ESXI_SHUTDOWN_MIN_REPEAT_PERIOD:
+            if not args.dry_run and now_epoch - last_shutdown < ESXI_SHUTDOWN_MIN_REPEAT_PERIOD:
                 logger.info("Skipping shutdown request as the last one was made {} seconds ago".format(now_epoch - last_shutdown))
                 continue
 
         # only allow shutdown if UPS is actively discharging
-        if not ups_is_discharging:
+        if not args.dry_run and not ups_stats['ups_is_discharging']:
             logger.info("Not making shutdown request: On AC power")
             continue
 
         # check if we need to make a shutdown request
-        if ups_pct_left > config[deployment]['initiate_shutdown_at_batt_pct_remaining']:
+        if ups_stats['ups_pct_left'] > config[deployment]['initiate_shutdown_at_batt_pct_remaining']:
             logger.info("Not making shutdown request: On battery power, but levels are still above {}%".format(
                 config[deployment]['initiate_shutdown_at_batt_pct_remaining']))
             continue
 
         logger.info("SHUTDOWN {}INITIATED - UPS has {}% left is < {}% allowed".format(
-            'DRY RUN ' if args.dry_run else '', ups_pct_left, config[deployment]['initiate_shutdown_at_batt_pct_remaining']))
+            'DRY RUN ' if args.dry_run else '', ups_stats['ups_pct_left'], config[deployment]['initiate_shutdown_at_batt_pct_remaining']))
 
-        if args.dry_run:
-            subcommand = "--operation info"
-        else:  # for real
-            subcommand = "--operation shutdown --force"
-        
-        env = os.environ.copy()
-        env['VI_PASSWORD'] = config[deployment]['esxi_password']
-        cmd = "vicfg-hostops --server {} --username {} {}".format(config[deployment]['esxi_host'], config[deployment]['esxi_username'], subcommand)
+        state[deployment]['shutdown-times'].append(now_epoch)  # log this time as a shutdown
 
-        child = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=env)
-        stdout_and_stderr = child.communicate()[0].decode("utf-8")  # wait for call to finish
-        rc = child.returncode
-        if rc != 0:
-            logger.info("Invalid response from vicfg-hostops: {}".format(stdout_and_stderr))
-        else:
-            logger.debug("SUCCESS - vicfg-hostops returned: {}".format(stdout_and_stderr))
+        # shut down the vcenter environment
+        state[deployment]['success'] = do_vcenter_shutdown(config[deployment], args.dry_run)
 
-        # log this time as a shutdown
-        state[deployment]['shutdown-times'].append(now_epoch)
+        # if this script is running on a VM in this vcenter deployment that is shutting down,
+        # we will need to shut down this system via shell command, as vcenter is already down (or shutting down)
+        if state[deployment]['success'] and config[deployment]['executing_host_vm_name']:
+            logger.info("Shutting down this system ({}) via shell command, in 1 minute (dry run: {})...".format(config[deployment]['executing_host_vm_name'], args.dry_run))
+            if not args.dry_run:
+                cmd = "shutdown -P +1"  # schedule shutdown for 1 minute out
+                child = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=os.environ.copy())
+                stdout_and_stderr = child.communicate()[0].decode("utf-8")  # wait for call to finish
+                rc = child.returncode
+                if rc != 0:
+                    logger.info("Invalid response from shutdown: {}".format(stdout_and_stderr))
+                    state[deployment]['success']  = False
     
     with open(state_file_path, 'w') as f:
         json.dump(state, f)
 
+    return all([deployment['success'] in (True, None) for deployment in state.values()])
+    
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(0 if main() == True else 1)
